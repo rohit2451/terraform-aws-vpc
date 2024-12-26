22ca2efdecef715301bfb88f370c1e2c9a30c829
rohit2451 <rohitredapangi@gmail.com>
2024-12-26 12:19:22 +0530

    terraform

--- peering.tf
+++ peering.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "peering" {
   count       = var.is_peering_required ? 1 : 0
   peer_vpc_id = data.aws_vpc.default.id #acceptor
   vpc_id      = aws_vpc.main.id         #requestor
